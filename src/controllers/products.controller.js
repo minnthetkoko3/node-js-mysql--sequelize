46b481d55f1550b5cdbad6ee15e3115234d39e8f
MgWunna <mail@wunna.info>
2022-11-24 11:33:01 +0630

    created create, read, update and delete controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,1 +1,38 @@
 import ProductsModel from '../models/products.model.js';
+
+const productController = {
+  create: async ({ title, price, imageUrl, description }) => {
+    const result = await ProductsModel.create({ title, price, imageUrl, description });
+    if (result) return result.dataValues;
+  },
+  findAll: async condition => {
+    const result = await ProductsModel.findAll(condition);
+    if (result) {
+      const products = result.map(data => data.dataValues);
+      return products;
+    }
+  },
+  findById: async productID => {
+    const result = await ProductsModel.findByPk(productID);
+    if (result) return result.dataValues;
+  },
+  findAndUpdate: async (productID, product) => {
+    const result = await ProductsModel.findByPk(productID);
+    if (result) {
+      result.title = product.title || result.title;
+      result.price = product.price || result.price;
+      result.imageUrl = product.imageUrl || result.imageUrl;
+      result.description = product.description || result.description;
+      return (await result.save()).dataValues;
+    }
+  },
+  delete: async productID => {
+    let result = await ProductsModel.findByPk(productID);
+    if (result) {
+      result = await result.destroy();
+      if (result) return true;
+    }
+  },
+};
+
+export default productController;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,62 @@
 import 'dotenv/config';
 import database from './configs/database.config.js';
-import {} from './controllers/products.controller.js';
+import productController from './controllers/products.controller.js';
 
 database
   .sync()
   .then(_ => {
     console.log('Connect mysql database success.');
+
+    // ✔ save product data
+    /*
+    productController
+      .create({
+        title: 'Some title',
+        price: 52.5,
+        imageUrl: 'https://localhost/images/some-image.jpg',
+        description: 'Some description',
+      })
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+    */
+
+    // ✔ find products data
+    /*
+    productController
+      .findAll()
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+    */
+
+    // ✔ find product data by condition
+    /*
+    productController
+      .findAll({ where: { id: 1 } })
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+    */
+
+    // ✔ find product data by id
+    /*
+    productController
+      .findById(1)
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
+    */
+
+    // ✔ find product and update by id
+    // productController
+    //   .findAndUpdate(1, { title: 'Hello' })
+    //   .then(data => console.log(data))
+    //   .catch(error => console.log(error));
+
+    // ✔ delete product by id
+    /* 
+    productController
+      .delete(3)
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+    */
   })
   .catch(error => {
     console.log(error);
